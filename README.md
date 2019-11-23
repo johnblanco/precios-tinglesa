e5ff4001f9fed579fd85edbebb96ff14299b3a44
Juan Blanco <jblanco@despegar.com>
2019-11-22 22:27:42 -0300

    lala

--- README.md
+++ README.md
@@ -2,8 +2,4 @@
 
 Bot que baja los precios de un conjunto de productos del super para posterior analisis. 
 
-ting.py es el que baja los precios, se puede correr con un cron diario con: 
 
-30 12 * * * /Users/jblanco/ting/ting.py
-
-analysis.py genera un csv de resumen
--- ting.py
+++ ting.py
@@ -1,4 +1,4 @@
-#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+#!/Users/juan/anaconda3/bin/python3
 import requests
 from bs4 import BeautifulSoup
 from datetime import date
@@ -11,18 +11,24 @@ def extract_price(str):
 
     return int(digits)
 
+def run():
+    path = '/Users/juan/projects/precios-tinglesa'
 
-f = open('/Users/jblanco/ting/result.csv', 'a')
-d = date.today().isoformat()
-products = open('/Users/jblanco/ting/products', 'r').readlines()
+    f = open(path + '/result.csv', 'a')
+    d = date.today().isoformat()
+    products = open(path + '/products', 'r').readlines()
 
-for url in products:
-    url = url.replace("\n","")
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    s = soup.find_all(id='TXTPRICE')[0]
-    price = extract_price(s.get_text())
+    for url in products:
+        url = url.replace("\n","")
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        s = soup.find_all(id='TXTPRICE')[0]
+        price = extract_price(s.get_text())
+        product_id = url.split('?')[1]
 
-    f.write('{},{},{}\n'.format(d, url, price))
+        f.write('{},{},{}\n'.format(d, product_id, price))
 
-f.close()
+    f.close()
+
+if __name__ == '__main__':
+    run()
